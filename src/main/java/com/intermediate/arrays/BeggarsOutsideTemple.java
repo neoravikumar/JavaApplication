320a638734e7b412c71cb775fa6a930ec926d35d
neoravikumar <neoravikumar@gmail.com>
2021-09-17 20:50:12 +0530

    commit 17 July

---
+++ src/main/java/com/intermediate/arrays/BeggarsOutsideTemple.java
@@ -0,0 +1,110 @@
+package com.intermediate.arrays;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ Beggars Outside Temple
+There are N (N > 0) beggars sitting in a row outside a temple. Each beggar initially has an empty pot. When the devotees come to the temple, they donate some amount of coins to these beggars. Each devotee gives a fixed amount of coin(according to his faith and ability) to some K beggars sitting next to each other.
+
+Given the amount donated by each devotee to the beggars ranging from i to j index, where 1 <= i <= j <= N, find out the final amount of money in each beggar's pot at the end of the day, provided they don't fill their pots by any other means.
+
+Example:
+
+Input:
+N = 5, D = [[1, 2, 10], [2, 3, 20], [2, 5, 25]]
+
+Return: [10, 55, 45, 25, 25]
+
+Explanation:
+=> First devotee donated 10 coins to beggars ranging from 1 to 2. Final amount in each beggars pot after first devotee: [10, 10, 0, 0, 0]
+
+=> Second devotee donated 20 coins to beggars ranging from 2 to 3. Final amount in each beggars pot after second devotee: [10, 30, 20, 0, 0]
+
+=> Third devotee donated 25 coins to beggars ranging from 2 to 5. Final amount in each beggars pot after third devotee: [10, 55, 45, 25, 25]
+ */
+public class BeggarsOutsideTemple {
+
+	public ArrayList<Integer> solve(int N, ArrayList<ArrayList<Integer>> B) {
+		ArrayList<Integer> beggerList = new ArrayList<Integer>();
+		if (N > 0) {
+			for (int i = 0; i < N; i++) {
+				beggerList.add(0);
+			}
+
+			for (int i = 0; i < B.size(); i++) {
+
+				int left = B.get(i).get(0) - 1;
+				int right = B.get(i).get(1) - 1;
+				int amount = B.get(i).get(2);
+
+				
+				if ((right + 1) < N) {
+					int finalamount2 = beggerList.get(right + 1) - amount;
+					beggerList.set(right + 1, finalamount2);
+				}
+
+				int finalamount = beggerList.get(left) + amount;
+				beggerList.set(left, finalamount);
+			}
+
+			for (int i = 1; i < N; i++) {
+				beggerList.set(i, beggerList.get(i - 1) + beggerList.get(i));
+			}
+
+		}
+		return beggerList;
+	}
+
+	public static void main(String[] args) {
+
+		BeggarsOutsideTemple beggarsOutsideTemple = new BeggarsOutsideTemple();
+
+		
+		  //A : 10 B : [ [1, 3, 10] [6, 9, 2] [3, 5, 3] [2, 8, 4] [6, 7, 5] ]
+		  
+		  
+		/*
+		 * int A = 10; ArrayList<Integer> devotee1 = new ArrayList<Integer>();
+		 * devotee1.add(1); devotee1.add(3); devotee1.add(10);
+		 * 
+		 * ArrayList<Integer> devotee2 = new ArrayList<Integer>(); devotee2.add(6);
+		 * devotee2.add(9); devotee2.add(2);
+		 * 
+		 * ArrayList<Integer> devotee3 = new ArrayList<Integer>(); devotee3.add(3);
+		 * devotee3.add(5); devotee3.add(3);
+		 * 
+		 * ArrayList<Integer> devotee4 = new ArrayList<Integer>(); devotee4.add(2);
+		 * devotee4.add(8); devotee4.add(4);
+		 * 
+		 * ArrayList<Integer> devotee5 = new ArrayList<Integer>(); devotee5.add(6);
+		 * devotee5.add(7); devotee5.add(5); ArrayList<ArrayList<Integer>> devoteeList =
+		 * new ArrayList<ArrayList<Integer>>(); devoteeList.add(devotee1);
+		 * devoteeList.add(devotee2); devoteeList.add(devotee3);
+		 * devoteeList.add(devotee4); devoteeList.add(devotee5);
+		 */
+		 
+		
+		
+		  int A=5; ArrayList<Integer> devotee1 = new ArrayList<Integer>();
+		  devotee1.add(1); devotee1.add(2); devotee1.add(10);
+		  
+		  ArrayList<Integer> devotee2 = new ArrayList<Integer>(); devotee2.add(2);
+		  devotee2.add(3); devotee2.add(20);
+		  
+		  ArrayList<Integer> devotee3 = new ArrayList<Integer>(); devotee3.add(2);
+		  devotee3.add(5); devotee3.add(25);
+		  
+		  
+		  ArrayList<ArrayList<Integer>> devoteeList = new
+		  ArrayList<ArrayList<Integer>>(); devoteeList.add(devotee1);
+		  devoteeList.add(devotee2); devoteeList.add(devotee3);
+		 
+
+		ArrayList<Integer> result = beggarsOutsideTemple.solve(A, devoteeList);
+		result.forEach(a -> System.out.println(a));
+
+	}
+
+}
---
+++ src/main/java/com/intermediate/strings/LongestCommonPrefix.java
@@ -0,0 +1,105 @@
+package com.intermediate.strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/*
+ Longest Common Prefix
+Problem Description
+
+Given the array of strings A, you need to find the longest string S which is the prefix of ALL the strings in the array.
+
+Longest common prefix for a pair of strings S1 and S2 is the longest string S which is the prefix of both S1 and S2.
+
+For Example: longest common prefix of "abcdefgh" and "abcefgh" is "abc".
+
+
+
+Problem Constraints
+0 <= sum of length of all strings <= 1000000
+
+
+
+Input Format
+The only argument given is an array of strings A.
+
+
+
+Output Format
+Return the longest common prefix of all strings in A.
+
+
+
+Example Input
+Input 1:
+
+A = ["abcdefgh", "aefghijk", "abcefgh"]
+Input 2:
+
+A = ["abab", "ab", "abcd"];
+
+
+Example Output
+Output 1:
+
+"a"
+Output 2:
+
+"ab"
+
+
+Example Explanation
+Explanation 1:
+
+Longest common prefix of all the strings is "a".
+Explanation 2:
+
+Longest common prefix of all the strings is "ab".
+ */
+public class LongestCommonPrefix {
+
+	public String longestCommonPrefix(ArrayList<String> A) {
+
+		int N = null != A ? A.size() : 0;
+		StringBuilder sb = new StringBuilder();
+
+		Collections.sort(A,  Comparator.comparingInt(String::length));
+		if (N >= 0 && N <= 1000000) {
+			boolean isNotAdd = false;
+
+			for (int i = 0; i < A.get(0).length(); i++) {
+
+				for (int k = 1; k < N; k++) {
+					if (A.get(0).charAt(i) != A.get(k).charAt(i)) {
+						isNotAdd = true;
+						break;
+					}
+				}
+				if (!isNotAdd) {
+					sb.append(A.get(0).charAt(i));
+				}
+			}
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		LongestCommonPrefix lcp = new LongestCommonPrefix();
+
+		List<String> arrays = Arrays.asList("abab", "abdc", "abcd");
+		//List<String> arrays = Arrays.asList("abcdefgh", "aefghijk", "abcefgh");
+		ArrayList<String> abc = new ArrayList<String>();
+		abc.addAll(arrays);
+		String result = lcp.longestCommonPrefix(abc);
+
+		System.out.println(result);
+
+	}
+
+}
